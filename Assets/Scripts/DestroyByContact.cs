8759fbb42512e65defda069695ac8bde4805f7f0
andrew <computedscientist@gmail.com>
2016-02-13 16:17:26 -0500

    Added asteroid object, game controller, and spawn wave

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+	// Add reference to explosion for the asteroid and player
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	void OnTriggerEnter(Collider other)
+	{
+		// Debug was used to determine what game object was destroying the asteroid
+		//Debug.Log(other.name);
+
+		// Return control to the caller
+		if(other.tag == "Boundary")
+		{
+			return;
+		}
+
+		// Show explosion at the position and with the rotation of the asteroid
+		Instantiate(explosion,transform.position,transform.rotation);
+
+		// Show player explosion if the player is detected
+		if(other.tag == "Player")
+		{
+			Instantiate(playerExplosion,other.transform.position,other.transform.rotation);
+		}
+
+		// Destroy the asteroid game object
+		Destroy(other.gameObject);
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	void Start()
+	{
+		StartCoroutine(spawnWaves());
+	}
+
+	// Generate hazards for the player
+	IEnumerator spawnWaves()
+	{
+		yield return new WaitForSeconds(startWait);
+
+		// Continuously spawn waves of hazards
+		while(true)
+		{
+			for(int i = 0;i < hazardCount;i++)
+			{
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x,spawnValues.x),spawnValues.y,spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+
+				Instantiate(hazard,spawnPosition,spawnRotation);
+
+				yield return new WaitForSeconds(spawnWait);
+			}
+
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+}
